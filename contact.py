0851398ec3a59998f9c3ac4428d5faf23dd007a9
Vanessa Hanna <99267778+VanessaHanna05@users.noreply.github.com>
2025-03-18 20:41:45 +0200

    commit 1001

--- contact.py
+++ contact.py
@@ -69,12 +69,12 @@ def app(navigate):
     col1, col2 = st.columns(2)
 
     with col1:
-        email = st.text_input(placeholder="Enter your email")
+        email = st.text_input("",placeholder="Enter your email")
 
     with col2:
-        name = st.text_input(placeholder="Enter your name")
+        name = st.text_input("",placeholder="Enter your name")
 
-    #message = st.text_area("Message", placeholder="Write your message here")
+    message = st.text_area("","Message", placeholder="Write your message here")
     
     if st.button("Submit"):
         if email and name and message:
