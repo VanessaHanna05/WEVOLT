4228f1766f7bf721d4b70b016b7c152f4ffb9339
Vanessa Hanna <99267778+VanessaHanna05@users.noreply.github.com>
2025-05-05 17:34:22 +0300

    fucking

--- login.py
+++ login.py
@@ -20,7 +20,10 @@ import sort_users
 #        firebase_admin.initialize_app(cred)
 #else:
 #    raise FileNotFoundError("Firebase credentials not found in Streamlit secrets.")
-cred = credentials.Certificate('wevolt-4d8a8-2e9079117595.json')
+#cred = credentials.Certificate('wevolt-4d8a8-2e9079117595.json')
+firebase_dict = json.loads(st.secrets["FIREBASE_CREDENTIALS"])
+cred = credentials.Certificate(firebase_dict)
+
 
 # Initialize Firebase Admin SDK
 if not firebase_admin._apps:
